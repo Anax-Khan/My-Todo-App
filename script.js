2a84a8fcf1d6a64e36202d7f0afc8f5ff9d214ac
Anax-Khan <ak4anas@gmail.com>
2023-03-29 18:07:44 +0500

    Added few code in JS file by which we store data in local storage

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //initialize array and a variable
+let addValue = JSON.parse(localStorage.getItem('key')) || [];
 
-let addValue = [];
 let final ="";
 
 //created a funtion that takes input from text field from user
-
 function saving(){
    const Input = document.getElementById("field");
    addValue.push(Input.value);
 
+//pushing data to local storage   
+   localStorage.setItem('key', JSON.stringify(addValue));
+
+//empties text field   
    Input.value ="";
 
 //made final variable empty so it does not repeats previus values
@@ -20,7 +23,7 @@ function saving(){
 for (i=0; i<addValue.length;i++)
 {
    final += `<li> ${addValue[i]} </li>`;
-
+   
    document.getElementById("data").innerHTML = final;
 }
 
