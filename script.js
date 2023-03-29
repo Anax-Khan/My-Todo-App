7e48b99a6ebcf7fc2c60139fda81dafea68a412c
Anax-Khan <ak4anas@gmail.com>
2023-03-29 14:08:50 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//initialize array and a variable
+
+let addValue = [];
+let final ="";
+
+//created a funtion that takes input from text field from user
+
+function saving(){
+   const Input = document.getElementById("field");
+   addValue.push(Input.value);
+
+   Input.value ="";
+
+//made final variable empty so it does not repeats previus values
+
+   final ="";
+
+//made a loop so that list get update
+
+for (i=0; i<addValue.length;i++)
+{
+   final += `<li> ${addValue[i]} </li>`;
+
+   document.getElementById("data").innerHTML = final;
+}
+
+//stops page from reload
+event.preventDefault();
+}
