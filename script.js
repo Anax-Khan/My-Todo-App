91407fedf07592492118cda046c217001a8f2d0b
Anax-Khan <ak4anas@gmail.com>
2023-04-01 13:16:33 +0500

    Added a button in list to remove done tasks

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //initialize array and a variable
 let addValue = JSON.parse(localStorage.getItem('key')) || [];
-
 let final ="";
 
 //created a funtion that takes input from text field from user
 function saving(){
    const Input = document.getElementById("field");
-   addValue.push(Input.value);
+   const valuePush =Input.value
+
+ //if there is any vlaue only when array will be updated  
+   if(valuePush){
+   addValue.push(valuePush);
+   }
 
 //pushing data to local storage   
    localStorage.setItem('key', JSON.stringify(addValue));
@@ -15,18 +19,40 @@ function saving(){
    Input.value ="";
 
 //made final variable empty so it does not repeats previus values
-
    final ="";
 
-//made a loop so that list get update
-
-for (i=0; i<addValue.length;i++)
-{
-   final += `<li> ${addValue[i]} </li>`;
-   
-   document.getElementById("data").innerHTML = final;
-}
+//function to print list
+print();
 
 //stops page from reload
 event.preventDefault();
+}
+
+
+
+
+//clear list
+function clr()
+{
+   addValue = []
+   localStorage.clear();
+   document.getElementById("data").innerHTML = ""  
+}
+
+//print button and list in Todo list
+function print() {
+   for (i=0; i<addValue.length;i++)
+   {
+      final += `<div class="horizon"><li class="ele"> ${addValue[i]} </li> <button class="dltBTn" id="delBtn${i}" onclick="addValue.splice(${i},1); saving(); lastCheck();" > Done </button><br> </div> `;
+      document.getElementById("data").innerHTML = final;
+   }  
+}
+
+//last one itself nahi ht raha tha to clr kraya
+function lastCheck() {
+
+   if(addValue.length === 0)
+   {
+      clr();
+   }
 }
